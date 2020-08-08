a9a4ceec113033a960adb51fa478806db4dcd757
Lucien Tsai <66447643+LucciAi@users.noreply.github.com>
2020-08-07 20:47:56 -0700

    Update API Response/Fixed Errors/Added User Input

--- traffy.py
+++ traffy.py
@@ -3,10 +3,10 @@ from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import ApiException
 
-API_Key='API_Key'
-Version='YYYY-MM-DD'
+API_Key='8dwm6jdHdEghcGIc6SULg6BOOkfGGA12ovPOC8hd5gvK'
+Version='2020-08-07'
 URL='https://api.us-south.assistant.watson.cloud.ibm.com'
-Assistant_ID='Assistant_ID'
+Assistant_ID='23c7e3a3-b5b3-4357-b2f8-d5cd002c316b'
 
 authenticator=IAMAuthenticator(API_Key)
 assistant=AssistantV2(
@@ -16,24 +16,37 @@ assistant=AssistantV2(
 
 assistant.set_service_url(URL)
 
-try:
-    response = assistant.create_session(
-        assistant_id=Assistant_ID
-    ).get_result()
+response=assistant.create_session(
+    assistant_id=Assistant_ID
+).get_result()
 
-    x=json.loads(json.dumps(response, indent=2))
-    Session_ID=x['session_id']
+x=json.loads(json.dumps(response, indent=2))
+Session_ID=x['session_id']
 
-    response = assistant.message(
-        assistant_id=Assistant_ID,
-        session_id=Session_ID,
-        input={
-            'message_type': 'text',
-            'text': 'Hello'
-        }
-    ).get_result()
+message_input='start_session'
 
-    y=json.loads(json.dumps(response, indent=2))
-    message_return=y['output']['generic']['text']
-except ApiException as ex:
-    print('Method failed with status code '' + str(ex.code) + ": " + ex.message)
+while message_input!='quit':
+    try:
+        message_input=input('Message: ')
+        response=assistant.create_session(
+            assistant_id=Assistant_ID
+        ).get_result()
+
+        x=json.loads(json.dumps(response, indent=2))
+        Session_ID=x['session_id']
+
+        response=assistant.message(
+            assistant_id=Assistant_ID,
+            session_id=Session_ID,
+            input={
+                'message_type': 'text',
+                'text': str(message_input)
+                }
+        ).get_result()
+
+        watson_response=json.loads(json.dumps(response, indent=2))
+        message_return=watson_response['output']['generic'][0]['text']
+        print(message_return)
+    except ApiException as ex:
+        print('Method failed with status code ' + str(ex.code) + ': ' + ex.message)
+        continue
