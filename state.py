083b6bbc3879d8400ce3ab7cdbfbd7081e4bda22
Lucien Tsai <66447643+LucciAi@users.noreply.github.com>
2020-08-08 16:13:47 -0700

    Function to change state name to id

---
+++ state.py
@@ -0,0 +1,106 @@
+def state_id(state_input):
+    if state_name=='Alabama':
+        state_id='AL'
+    elif state_name=='Alaska':
+        state_id='AK'
+    elif state_name=='Arizona':
+        state_id='AZ'
+    elif state_name=='Arkansas':
+        state_id='AR'
+    elif state_name=='California':
+        state_id='CA'
+    elif state_name=='Colorado':
+        state_id='CO'
+    elif state_name=='Connecticut':
+        state_id='CT'
+    elif state_name=='Delaware':
+        state_id='DE'
+    elif state_name=='District of Columbia':
+        state_id='DC'
+    elif state_name=='Florida':
+        state_id='FL'
+    elif state_name=='Georgia':
+        state_id='GA'
+    elif state_name=='Hawaii':
+        state_id='HI'
+    elif state_name=='Idaho':
+        state_id='ID'
+    elif state_name=='Illinois':
+        state_id='IL'
+    elif state_name=='Indiana':
+        state_id='IN'
+    elif state_name=='Iowa':
+        state_id='IA'
+    elif state_name=='Kansas':
+        state_id='KS'
+    elif state_name=='Kentucky':
+        state_id='KY'
+    elif state_name=='Louisiana':
+        state_id='LA'
+    elif state_name=='Maine':
+        state_id='ME'
+    elif state_name=='Maryland':
+        state_id='MD'
+    elif state_name=='Massachusetts':
+        state_id='MA'
+    elif state_name=='Michigan':
+        state_id='MI'
+    elif state_name=='Minnesota':
+        state_id='MN'
+    elif state_name=='Mississippi':
+        state_id='MS'
+    elif state_name=='Missouri':
+        state_id='MO'
+    elif state_name=='Montana':
+        state_id='MT'
+    elif state_name=='Nebraska':
+        state_id='NE'
+    elif state_name=='Nevada':
+        state_id='NV'
+    elif state_name=='New Hampshire':
+        state_id='NH'
+    elif state_name=='New Jersey':
+        state_id='NJ'
+    elif state_name=='New Mexico':
+        state_id='NM'
+    elif state_name=='New York':
+        state_id='NY'
+    elif state_name=='North Carolina':
+        state_id='NC'
+    elif state_name=='North Dakota':
+        state_id='ND'
+    elif state_name=='Ohio':
+        state_id='OH'
+    elif state_name=='Oklahoma':
+        state_id='OK'
+    elif state_name=='Oregon':
+        state_id='OR'
+    elif state_name=='Pennsylvania':
+        state_id='PA'
+    elif state_name=='Rhode Island':
+        state_id='RI'
+    elif state_name=='South Carolina':
+        state_id='SC'
+    elif state_name=='South Dakota':
+        state_id='SD'
+    elif state_name=='Tennessee':
+        state_id='TN'
+    elif state_name=='Texas':
+        state_id='TX'
+    elif state_name=='Utah':
+        state_id='UT'
+    elif state_name=='Vermont':
+        state_id='VT'
+    elif state_name=='Virginia':
+        state_id='VA'
+    elif state_name=='Washington':
+        state_id='WA'
+    elif state_name=='West Virginia':
+        state_id='WV'
+    elif state_name=='Wisconsin':
+        state_id='WI'
+    elif state_name=='Wyoming':
+        state_id='WY'
+    else:
+        state_id='00'
+    return state_id
